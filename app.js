5dd42ddaa0f000bb2887848acb1483b35656b2ca
Barahalikar Siddharth <barahalikar.siddharth@gmail.com>
2023-08-21 01:25:53 +0530

    asd

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect("mongodb://process.env.MONGO_HOST:process.env.MONGO_PORT/superData", {
     user: process.env.MONGO_USERNAME,
     pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
