4e749c40cb90d71fad69bed72abbade4d2611f51
Barahalikar Siddharth <barahalikar.siddharth@gmail.com>
2023-08-24 00:40:12 +0530

    [skip ci]

---
+++ test-script.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+apt intall -y jq lolcat fortune boxes
+# set -e
+
+# # setting the input message to a variable
+# if [ "$INPUT_MESSAGE" == "" ]; then
+#     message="randomText"
+#   else
+#     message=$INPUT_MESSAGE
+# fi
+
+#   if [ "$message" == "randomText" ]; then
+#     result=$(fortune | boxes -a c -d parchment | lolcat)
+#   else
+#     result=$(echo "$message" | | boxes -a c -d parchment | lolcat)
+#   fi
+
+
+
+  # Comment on the PR
+      URL=$(cat $GITHUB_EVENT_PATH | jq -r .pull_request.comments_url)
+    #   commentFromCowsay="\`\`\`
+    #     ${result}
+    #     \`\`\`
+    #     "
+      payload=$(fortune | boxes -a c -d parchment | lolcat)
+      echo "${payload}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "$URL" > /dev/null
+    
